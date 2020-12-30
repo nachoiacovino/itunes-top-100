30a5ada6ef6699d6840aa7b9e024ef312e0137fd
Nacho Iacovino <nacho.iacovino@gmail.com>
2020-12-30 12:30:22 +0100

    Install react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Col, Container, Row } from 'react-bootstrap';
+
 const App = () => {
-  return <div>Test</div>;
+  return (
+    <Container>
+      <Row>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default App;
