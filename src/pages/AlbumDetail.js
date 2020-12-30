63c112943a7a33e11c728ab618231b83a2ced4c5
Nacho Iacovino <nacho.iacovino@gmail.com>
2020-12-30 16:50:42 +0100

    Add option to add/remove favorite on AlbumDetail

--- src/pages/AlbumDetail.js
+++ src/pages/AlbumDetail.js
@@ -1,11 +1,12 @@
 import { useEffect } from 'react';
 import { Button, Col, Container, Image, Row } from 'react-bootstrap';
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import Error from '../components/Error';
 import Loading from '../components/Loading';
-import { getAlbumsStart } from '../redux/albums/albumsActions';
+import { addAlbumToFavorites, getAlbumsStart, removeAlbumFromFavorites } from '../redux/albums/albumsActions';
 import NotFound from './NotFound';
 
 const AlbumDetail = () => {
@@ -16,6 +17,13 @@ const AlbumDetail = () => {
   );
   const loading = useSelector(({ albums }) => albums.loading);
   const error = useSelector(({ albums }) => albums.error);
+
+  const handleFavorite = (action) => {
+    const albumId = album.id.attributes['im:id'];
+
+    if (action === 'ADD') dispatch(addAlbumToFavorites(albumId));
+    else if (action === 'REMOVE') dispatch(removeAlbumFromFavorites(albumId));
+  };
 
   useEffect(() => {
     dispatch(getAlbumsStart());
@@ -36,21 +44,33 @@ const AlbumDetail = () => {
         </Col>
         <Col xs={12} md={9}>
           <h1>{album['im:name'].label}</h1>
+
           <h3>{album['im:artist'].label}</h3>
           <p className='m-0'>Price: {album['im:price'].label}</p>
           <p className='mt-0'>
             Release date: {formatDate(album['im:releaseDate'].label)}{' '}
           </p>
-          <Button className='mb-4'>
-            <a
-              className='text-white'
-              href={album.link.attributes.href}
-              target='_blank'
-              rel='noreferrer'
-            >
-              Buy on iTunes
-            </a>
-          </Button>
+          <div className='mb-4'>
+            {album.favorite ? (
+              <Button onClick={() => handleFavorite('REMOVE')}>
+                <AiFillStar />
+              </Button>
+            ) : (
+              <Button onClick={() => handleFavorite('ADD')}>
+                <AiOutlineStar />
+              </Button>
+            )}
+            <Button className='ml-2'>
+              <a
+                className='text-white'
+                href={album.link.attributes.href}
+                target='_blank'
+                rel='noreferrer'
+              >
+                Buy on iTunes
+              </a>
+            </Button>
+          </div>
           <p>
             <small>{album.rights.label}</small>
           </p>
